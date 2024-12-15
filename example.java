d9f19acbc5ea8be3f8003e331afc0fbd91608df0
abhinay <abhinayeddy240>
2024-12-15 15:09:08 +0530

    docker example

---
+++ example.java
@@ -0,0 +1,30 @@
+public class example {
+    public static void main(String[] args) {
+        int rows = 5; // Number of rows
+        // Upper part of the diamond
+        for (int i = 1; i <= rows; i++) {
+            // Print spaces
+            for (int j = i; j < rows; j++) {
+                System.out.print(" ");
+            }
+            // Print stars
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println(); // Move to the next line
+        }
+        // Lower part of the diamond
+        for (int i = rows - 1; i >= 1; i--) {
+            // Print spaces
+            for (int j = rows; j > i; j--) {
+                System.out.print(" ");
+            }
+            // Print stars
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println(); // Move to the next line
+        }
+    }
+}
+
